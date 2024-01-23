cd40f11fb52f71d9cb62899ba053d1fc7ce7a887
GGTuran <tayyabturan67@gmail.com>
2024-01-23 10:29:25 +0600

    installed tailwind and daisyui for practise

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+
+      <div>
+      <div className="stats shadow">
+  
+  <div className="stat place-items-center">
+    <div className="stat-title">Downloads</div>
+    <div className="stat-value">31K</div>
+    <div className="stat-desc">From January 1st to February 1st</div>
+  </div>
+  
+  <div className="stat place-items-center">
+    <div className="stat-title">Users</div>
+    <div className="stat-value text-secondary">4,200</div>
+    <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+  </div>
+  
+  <div className="stat place-items-center">
+    <div className="stat-title">New Registers</div>
+    <div className="stat-value">1,200</div>
+    <div className="stat-desc">↘︎ 90 (14%)</div>
+  </div>
+  
+</div>
+      </div>
+      
+
+      <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
+  <nav>
+    <header className="footer-title">Services</header> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Legal</header> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav> 
+  <form>
+    <header className="footer-title">Newsletter</header> 
+    <fieldset className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label> 
+      <div className="join">
+        <input type="text" placeholder="username@site.com" className="input input-bordered join-item" /> 
+        <button className="btn btn-primary join-item">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+      </div>
+    </>
+  )
+}
+
+export default App
