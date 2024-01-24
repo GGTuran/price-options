a0b1e2c2a5fa9cfd0b849521a037997502925e7d
GGTuran <tayyabturan67@gmail.com>
2024-01-24 11:14:55 +0600

    showed gymbro lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Linechart from './Components/Linechart/Line'
 import Navbar from './Components/Navbar/Navbar'
+import PriceOptions from './Components/PriceOptions/PriceOptions'
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
     <h1>Dunno what i am doing</h1>
 
     <Navbar></Navbar>
+    <PriceOptions></PriceOptions>
     
     </>
   )
---
+++ src/Components/Linechart/Line.jsx
@@ -0,0 +1,23 @@
+import { LineChart, Line } from 'recharts';
+
+
+const Line = () => {
+    const data = [
+        { id: 1, studentName: "Alice", marks: 85 },
+        { id: 2, studentName: "Bob", marks: 78 },
+        { id: 3, studentName: "Charlie", marks: 92 },
+        { id: 4, studentName: "David", marks: 65 },
+        { id: 5, studentName: "Eva", marks: 89 },
+        { id: 6, studentName: "Frank", marks: 73 },
+    ];
+    
+    return (
+     <div>
+            <Line data={}>
+
+            </Line>
+     </div>
+    );
+};
+
+export default Linechart;
